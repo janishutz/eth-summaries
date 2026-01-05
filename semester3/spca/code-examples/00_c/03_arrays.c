80997f5c15706b387b261c63481d1680a8cb43a3
Janis Hutz <development@janishutz.com>
2026-01-05 18:10:12 +0100

    [SPCA] Start arrays overview

--- semester3/spca/code-examples/00_c/03_arrays.c
+++ semester3/spca/code-examples/00_c/03_arrays.c
@@ -1,3 +1,15 @@
+#include <stdint.h>
+#include <stdio.h>
+
 int main( int argc, char *argv[] ) {
+    int data[ 10 ];                  // Initialize array of 10 integers
+    data[ 5 ] = 5;                   // element 5 is now 5
+    *data = 10;                      // element 0 is now 5
+    printf( "%d\n", data[ 0 ] );     // print element 0 (prints 10)
+    printf( "%d\n", *data );         // equivalent as above
+    printf( "%d\n", data[ 5 ] );     // print element 5 (prints 5)
+    printf( "%d\n", *( data + 5 ) ); // equivalent as above
+    int multidim[ 5 ][ 5 ];          // 2-dimensional array
+                                     // We can iterate over it using two for-loops
     return 0;
 }
