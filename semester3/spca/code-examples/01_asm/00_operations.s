58b70a2ec68cdef60ed1a461448d69f12cb51894
Janis Hutz <development@janishutz.com>
2026-01-11 08:36:55 +0100

    [SPCA] Fix some errors, add example

--- semester3/spca/code-examples/01_asm/00_operations.s
+++ semester3/spca/code-examples/01_asm/00_operations.s
@@ -1,2 +1,6 @@
 main:
-	addl 8(%rbp), %eax # Add long
+	movq %rax, %rdx # rax is src, rdx is dest
+	cmp  %rax, %rdx # rax is src2, rdx is src1
+	jmp  func       # func is label
+	notq %rax       # rax is dest (and src)
+	ret  # No argument
