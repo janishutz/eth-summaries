acffe6b3e6666fd513c15aa2b83204bc57c8db7d
Janis Hutz <development@janishutz.com>
2026-01-11 08:05:05 +0100

    [SPCA] Format

--- semester3/spca/code-examples/01_asm/02_max.s
+++ semester3/spca/code-examples/01_asm/02_max.s
@@ -1,9 +1,9 @@
 max:
-    cmpl %esi, %edi     // Set condition flags
-    jle .IF             // Conditional jump if %edi <= %esi
-    movl %edi, %edx     // %edi -> return register
+    cmpl %esi, %edi     # Set condition flags
+    jle .IF             # Conditional jump if %edi <= %esi
+    movl %edi, %edx     # %edi -> return register
     jmp .ELSE
 .IF:
-    movl %esi, %edx     // &esi -> return register
+    movl %esi, %edx     # &esi -> return register
 .ELSE:
     ret
