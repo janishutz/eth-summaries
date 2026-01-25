0770c76ef3e597fb11cbd329ed42582a2dfd87b6
Janis Hutz <development@janishutz.com>
2026-01-25 08:11:16 +0100

    [SPCA] Fixes a few errors

--- semester3/spca/code-examples/00_c/00_basics/03_arrays.c
+++ semester3/spca/code-examples/00_c/00_basics/03_arrays.c
@@ -4,7 +4,7 @@
 int main( int argc, char *argv[] ) {
     int data[ 10 ];                  // Initialize array of 10 integers
     data[ 5 ] = 5;                   // element 5 is now 5
-    *data = 10;                      // element 0 is now 5
+    *data = 10;                      // element 0 is now 10
     printf( "%d\n", data[ 0 ] );     // print element 0 (prints 10)
     printf( "%d\n", *data );         // equivalent as above
     printf( "%d\n", data[ 5 ] );     // print element 5 (prints 5)
---
+++ semester3/spca/code-examples/00_c/01_preprocessor/00_include-guards.h
@@ -0,0 +1,5 @@
+#ifndef MYLIB_H
+    #define MYLIB_H
+
+    // all code goes now here
+#endif
